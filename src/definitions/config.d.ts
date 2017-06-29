739bd30bd427438b01fb50fdf9e3cd54e79ede1a
Elvis Munoz <emunoz@andain.cl>
2017-06-29 15:00:29 -0400

    Renamed

--- src/definitions/config.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Config
-{
-    authorization_server: string;
-    resource_server: string;
-    application_identifier: string;
-    application_secret: string;
-    default_scopes: Array<string>;
-}
