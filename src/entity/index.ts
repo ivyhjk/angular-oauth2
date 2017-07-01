367fba8dc746723136a8bf6c2c1f84e98fb91c00
ivyhjk <elvis.munoz.f@gmail.com>
2017-07-01 16:01:42 -0300

    Exports credential entity

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -1,5 +1,6 @@
 export {AccessToken} from './access.token';
 export {Config} from './config';
+export {Credential} from './credential';
 export {RefreshToken} from './refresh.token';
 export {Token} from './token';
 export {TokenDefinition} from './token.definition';
