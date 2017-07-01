87e9fef0a86db1643ca309bdeb21c0de5dae5914
ivyhjk <elvis.munoz.f@gmail.com>
2017-07-01 16:03:24 -0300

    Remove unnecessary attributes for abstract token

--- src/entity/token.ts
+++ src/entity/token.ts
@@ -10,27 +10,25 @@ export abstract class Token
     private _identifier: null|string = null;
 
     /**
-     * The expiry time, in seconds.
-     *
-     * @var mixed[null|number]
-     */
-    private _expiry_time: null|number = null;
-
-    /**
-     * Creation date.
-     *
-     * @var Date
-     */
-    private _created_at: Date;
-
-    /**
      * Generate a new token.
      *
      * @param oauth2.client.entity.TokenDefinition
+     *
+     * @return void
      */
     public constructor(private _tokenDefinition: TokenDefinition)
     {
-        this._created_at = new Date();
+
+    }
+
+    /**
+     * Get the token definition.
+     *
+     * @return oauth2.client.entity.TokenDefinition
+     */
+    public get tokenDefinition() : TokenDefinition
+    {
+        return this._tokenDefinition;
     }
 
     /**
@@ -45,29 +43,5 @@ export abstract class Token
         }
 
         return this._identifier;
-    }
-
-    /**
-     * Get the expiry time, in seconds.
-     *
-     * @return number
-     */
-    public get expiry_time() : number
-    {
-        if (this._expiry_time === null) {
-            this._expiry_time = this._tokenDefinition.expiry_time;
-        }
-
-        return this._expiry_time;
-    }
-
-    /**
-     * Get the created at date.
-     *
-     * @return Date
-     */
-    public get created_at() : Date
-    {
-        return this._created_at;
     }
 }
